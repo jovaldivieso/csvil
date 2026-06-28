0079957d47c5e6537d65fcba4eb98468cc6d0bb5
jovaldivieso <j.m.valdivieso@icloud.com>
2026-06-28 22:55:59 +0200

    style: add comments to planner

--- planning/single_robot_casadi.py
+++ planning/single_robot_casadi.py
@@ -17,46 +17,65 @@ class SingleRobotCasadiPlanner:
         self.Q = np.diag(config.get("Q_diag", [10.0, 10.0, 1.0, 1.0]))
         self.R = np.eye(self.sim.nu) * config.get("R_weight", 0.1)
 
+        # Build the CasADi NLP graph
         self.opti = ca.Opti()
+
+        # State and action variables over the horizon
         self.X = self.opti.variable(self.sim.nx, self.N + 1)
         self.U = self.opti.variable(self.sim.nu, self.N)
+
+        # Initial state parameter (updated at runtime)
         self.x0_param = self.opti.parameter(self.sim.nx)
 
         goal_vec = ca.vertcat(self.goal[0], self.goal[1], 0.0, 0.0)
         cost = 0
 
+        # Build constraints and costs
         for k in range(self.N):
+            # Dynamics constraint (State Evolution)
             self.opti.subject_to(self.X[:, k + 1] ==
                                  self.sim.casadi_dynamics(self.X[:, k],
                                                           self.U[:, k]))
 
+            # Actuator limits (Control Bounds)
             for i in range(self.sim.nu):
                 self.opti.subject_to(self.U[i, k] >= -self.sim.max_action)
                 self.opti.subject_to(self.U[i, k] <= self.sim.max_action)
 
+            # Running cost (minimize distance to goal and control effort)
             error = self.X[:, k] - goal_vec
             cost += ca.mtimes(error.T, ca.mtimes(self.Q, error))
             cost += ca.mtimes(self.U[:, k].T, ca.mtimes(self.R, self.U[:, k]))
 
+        # Terminal cost (heavily penalize missing the goal at the end of
+        # horizon)
         terminal_error = self.X[:, self.N] - goal_vec
         cost += ca.mtimes(terminal_error.T, ca.mtimes(self.Q * 10,
                                                       terminal_error))
 
         self.opti.minimize(cost)
+
+        # Constrain the first state to exactly match our runtime parameter
         self.opti.subject_to(self.X[:, 0] == self.x0_param)
 
+        # Configure IPOPT solver (silence outputs for fast inference)
         opts = {"ipopt.print_level": 0, "print_time": 0, "ipopt.sb": "yes"}
         self.opti.solver("ipopt", opts)
 
     def __call__(self, obs):
+        # Reconstruct the global state [x, y, vx, vy] from the goal-relative
+        # observation
         pos_x = self.goal[0] - obs[0]
         pos_y = self.goal[1] - obs[1]
         x0 = np.array([pos_x, pos_y, obs[2], obs[3]])
 
+        # Inject the current state into the pre-compiled graph parameter
         self.opti.set_value(self.x0_param, x0)
 
         try:
+            # Solve the NLP and return the first optimal action step
             sol = self.opti.solve()
             return sol.value(self.U[:, 0])
         except RuntimeError:
+            # Return zero action if the solver fails to converge
             return np.zeros(self.sim.nu)
